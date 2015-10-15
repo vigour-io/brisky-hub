fd8fcb1961cce8a92299a6b5532c696de1e88e32
Jim de Beer <jim@vigour.io>
2015-10-15 14:59:32 +0200

    added the real example

--- examples/basic/index.js
+++ examples/basic/index.js
@@ -3,11 +3,24 @@ var Hub = require('../../lib/')
 // var b = new Hub({})
 
 
+
+
+
 var a = new Hub({
   key: 'myHubA',
   upstream: {
     adapter: {
-      val:'hello',
+      inject: require('../../lib/adapter/protocol/mock')
+    }
+  }
+})
+
+
+var b = new Hub({
+  key: 'myHubB',
+  upstream: {
+    val: a,
+    adapter: {
       inject: require('../../lib/adapter/protocol/mock')
     }
   }
@@ -18,12 +31,14 @@ console.log('fucking a', a)
 // a.on(fun)
 
 
-a.subscribe({
+b.subscribe({
   b: true
 }, function() {
   console.error('xxxx---xxxx')
   //eg: set on parent
 })
+
+a.set('b', 'bValue')
 
 //create new subs emitter if its not attached to _on
 //do a set on _on _on.set({ [subshash]: subsemitter })
