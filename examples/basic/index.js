316c42d749af8967939c7f8535127dec937fcd47
Jim de Beer <jim@vigour.io>
2015-10-16 10:30:26 +0200

    start of websocket example protocol

--- examples/basic/index.js
+++ examples/basic/index.js
@@ -1,5 +1,4 @@
 var Hub = require('../../lib/')
-
 // var b = new Hub({})
 
 var a = new Hub({
@@ -8,45 +7,3 @@ var a = new Hub({
     inject: require('../../lib/adapter/protocol/mock')
   }
 })
-
-// a.adapter.val = false
-
-
-var b = new Hub({
-  key: 'myHubB',
-  adapter: {
-    inject: require('../../lib/adapter/protocol/mock')
-  }
-})
-
-b.adapter.val = a
-
-console.log(b)
-
-console.log('fucking a', a)
-
-// a.on(fun)
-
-b.subscribe({
-  b: true
-}, function () {
-  console.error('xxxx---xxxx')
-  // eg: set on parent
-})
-
-console.error('hey im setting b on a!')
-
-setTimeout(function() {
-  a.set({
-    b: true
-  })
-},1000)
-
-
-//create new subs emitter if its not attached to _on
-//do a set on _on _on.set({ [subshash]: subsemitter })
-
-
-// a.set({
-//   b: '?'
-// })
