905558f6877120d246bd9fc91afd435dcc146f04
Jim de Beer <jim@vigour.io>
2015-10-16 10:10:47 +0200

    comments, logs

---
+++ test/common/all/index.js
@@ -0,0 +1,16 @@
+describe('create hubs', function () {
+  var Hub = require('../../../lib')
+
+  it('can create a hub', function () {
+    var a = new Hub()
+    expect(a).to.be.ok
+  })
+
+
+  // instanceID
+
+
+  // it('')
+
+
+})
