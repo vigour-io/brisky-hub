2606e8c8ca81c2ebbac25f71c0208b199990575a
Jim de Beer <jim@vigour.io>
2015-10-16 12:12:26 +0200

    first 2 way comunication

--- examples/basic/client.js
+++ examples/basic/client.js
@@ -9,3 +9,12 @@ var a = new Hub({
 })
 
 a.adapter.val = 'ws://localhost:3030'
+
+// function sendNumber () {
+//   if (client.readyState === client.OPEN) {
+//     var number = Math.round(Math.random() * 0xFFFFFF)
+//     client.send(number.toString())
+//     setTimeout(sendNumber, 1000)
+//   }
+// }
+// sendNumber()
