7193fa1f6c061f666763026f62b50271092905bb
Jim de Beer <jim@vigour.io>
2015-10-21 20:45:03 +0200

    added browser example /w styling

--- examples/browser/client.js
+++ examples/browser/client.js
@@ -11,20 +11,24 @@ var msgCount = new Observable(0)
 var Hub = require('../../lib')
 var ui = require('../ui')
 var start = Date.now()
-setInterval(function () {
-  start = Date.now()
-}, 1000)
 var now = new Observable(0)
 var connected = new Observable(false)
-setInterval(() => now.val++, 5000)
+var uuid = require('vjs/lib/util/uuid').val
+setInterval(() => {
+  now.val++
+}, 1000)
 
 var hub = global.hub = new Hub({
   adapter: {
     inject: require('../../lib/adapter/websocket'),
     on: {
-      connection (err) {
-        connected.val = err ? true : false
-        console.log('CONNECTION', err)
+      connection () {
+        connected.val = true
+        console.log('CONNECTION')
+      },
+      close () {
+        connected.val = false
+        console.log('close')
       },
       error (err) {
         console.error('err!', err)
@@ -44,13 +48,13 @@ var hub = global.hub = new Hub({
 setTimeout(() => hub.adapter.val = 3031, 200)
 setTimeout(() => hub.set({bla: 'something!'}), 300)
 
-global.app = new Element({
+var app = global.app = new Element({
   node: document.body,
   css: 'app',
   holder: {
     labels: {
       ChildConstructor: ui.Label,
-      uuid: { text: require('vjs/lib/util/uuid').val },
+      uuid: { text: uuid },
       start: {
         text: {
           val: start,
@@ -62,15 +66,16 @@ global.app = new Element({
           val: now,
           inject: require('../ui/transform/time')
         }
-      }
+      },
+      upstream: { text: hub.adapter }
     },
     // dit wil je eigenlijk gewoon supporten!
     msgcnt: new ui.Stat({
       title: { text: 'Messages' },
       counter: {
         text: {
-          inject: require('vjs/lib/operator/transform'),
-          val: msgCount,
+          inject: require('../ui/transform/number'),
+          val: msgCount
         }
       }
     }),
@@ -80,16 +85,14 @@ global.app = new Element({
         text: {
           inject: require('vjs/lib/operator/transform'),
           val: msgCount,
-          $transform: (val) => ~~((val / ((Date.now() - start))) / 100) / 10
+          $transform: (val) => ~~((val / ((Date.now() - start)))*1000)
         }
       }
     }),
     clients: new ui.Stat({
       status: {
         inject: require('vjs/lib/operator/transform'),
-        $transform (val) {
-          return val ? 'ok' : 'error'
-        },
+        $transform: (val) => val ? 'ok' : 'error',
         val: connected
       },
       title: { text: 'Clients' },
@@ -108,8 +111,35 @@ global.app = new Element({
   }
 })
 
+var updating = new Observable(false)
+updating.on(function (data) {
+  clearInterval(this._interval)
+  this._interval = null
+  if (this.val === true) {
+    this._interval = setInterval(() => {
+      hub.set({ text: (~~(Math.random() * 100)) + uuid })
+    }, 0)
+  }
+})
+
 app.holder.set({
-  button: new ui.Button({ text: 'Reconnect' }),
   input: new ui.Input({ input: { text: hub.get('text', {}) } }),
-  input2: new ui.Input({ input: { text: hub.get('text', {}) } })
+  // input2: new ui.Input({ input: { text: hub.get('text', {}) } }),
+  button: new ui.Button({
+    text: {
+      inject: require('vjs/lib/operator/transform'),
+      val: updating,
+      $transform: (val) => val === true ? 'Stop updates' : 'Fire updates'
+    },
+    css: {
+      inject: require('vjs/lib/operator/transform'),
+      val: updating,
+      $transform: (val) => val === true ? 'ui-button active' : 'ui-button inactive'
+    },
+    on: {
+      click () {
+        updating.val = !updating.val
+      }
+    }
+  })
 })
