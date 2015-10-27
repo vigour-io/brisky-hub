0f54ce384c188834a1977f2e1f48dfc4c8324bd3
Jim de Beer <jim@vigour.io>
2015-10-27 15:36:02 +0100

    added start of instance updates
    Former-commit-id: 7162aea4a29fec8ecf0d990e4ea65d475b990648

--- examples/instances/clientInstance.js
+++ examples/instances/clientInstance.js
@@ -1,7 +1,11 @@
 'use strict'
 var uuid = require('vjs/lib/util/uuid')
-uuid.val = 'NO_INSTANCE_' + uuid.val
+uuid.val = 'INSTANCE_A_' + uuid.val
 var hub = require('../dev/ui').hub
+// ofcourse we need reconnection strategies here WIP!
+
+hub.adapter.instance.val = 'a'
+
 setTimeout(() => {
   hub.adapter.val = 3031
 }, 100)
