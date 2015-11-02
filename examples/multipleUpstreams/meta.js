b325bfd8f188d3737a4dcb03757579f0d8354dde
Jim de Beer <jim@vigour.io>
2015-11-02 15:18:39 +0100

    added multi upstream example and start

---
+++ examples/multipleUpstreams/meta.js
@@ -0,0 +1,12 @@
+'use strict'
+var uuid = require('vjs/lib/util/uuid')
+uuid.val = 'meta_' + uuid.val
+var Hub = require('../../lib')
+var hub = global.hub = new Hub({
+  trackInstances: true,
+  adapter: {
+    inject: require('../../lib/adapter/websocket')
+  }
+})
+require('../basic/dev').startRepl()
+hub.adapter.listens.val = 3032
