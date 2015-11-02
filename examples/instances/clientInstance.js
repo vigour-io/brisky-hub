f27437dc21c41413f2bbe8988ac0eaa806951425
Jim de Beer <jim@vigour.io>
2015-11-02 15:49:31 +0100

    renamed instances to scope

--- examples/instances/clientInstance.js
+++ examples/instances/clientInstance.js
@@ -3,9 +3,5 @@ var uuid = require('vjs/lib/util/uuid')
 uuid.val = 'INSTANCE_A_' + uuid.val
 var hub = require('../dev/ui').hub
 // ofcourse we need reconnection strategies here WIP!
-
-hub.adapter.instance.val = 'a'
-
-setTimeout(() => {
-  hub.adapter.val = 3031
-}, 100)
+hub.adapter.scope.val = 'a'
+hub.adapter.val = 3031
