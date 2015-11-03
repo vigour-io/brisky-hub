c2cc100af71a834c32ab40b71332d7515731031a
Jim de Beer <jim@vigour.io>
2015-11-03 20:31:59 +0100

    removed logs, cleaned up added multi upstream example

--- examples/instances/client33.js
+++ examples/instances/client33.js
@@ -1,26 +1,14 @@
 'use strict'
 
 var uuid = require('vjs/lib/util/uuid')
-uuid.val = '3031_NORMAL_CLIENT_' + uuid.val
-
+uuid.val = '3033_NORMAL_CLIENT_' + uuid.val
 // var Hub = require('../../lib')
 // Hub.prototype.inject(require('vjs/lib/observable/storage'))
-
 var hub = require('../dev/ui').hub
 // var app = require('../dev/ui').app
 var Observable = require('vjs/lib/observable')
 hub.key = 'hubs'
-var a = new Observable({
-  key: 'a',
-  val: 0,
-  inject: require('vjs/lib/observable/storage')
-})
-
-setInterval(() => a.val++, 3000)
-setTimeout(() => hub.adapter.val = 3031, 100)
-
-//adapter should not rly update its clients? -- no just not updates when it allrdy running
-
+setTimeout(() => hub.adapter.val = 3033, 100)
 // setInterval(() => {
 //   // hub.set({
 //   //   text: 'no scope 3031'
