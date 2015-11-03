3b821c9214dd4314b9e2c4dd1be68e00d4d77148
Jim de Beer <jim@vigour.io>
2015-11-03 13:57:30 +0100

    scope resolvement to upstreams - needs perf optmization - needs to be split up correctly
    Former-commit-id: 3356fd0370e45259ea63dd2117cb601b3d1c243a

--- examples/multipleUpstreams/index.js
+++ examples/multipleUpstreams/index.js
@@ -3,33 +3,25 @@ var uuid = require('vjs/lib/util/uuid')
 uuid.val = 'server_' + uuid.val
 var Hub = require('../../lib')
 var hub = global.hub = new Hub({
-  trackInstances: true,
   adapter: {
     inject: require('../../lib/adapter/websocket')
-  }
+  },
+  blurf: true
 })
-require('../basic/dev').startRepl()
-// hub.set({clients:{}})
-hub.adapter.listens.val = 3031
 
-var scopes = Hub.prototype.scopes
+require('../basic/dev').startRepl()
+hub.adapter.listens.val = 3031
+var _scopes = Hub.prototype.scopes
+
 hub.define({
   scopes (scope, event) {
-    var instance = scopes.call(this, scope, event)
+    var instance = _scopes.call(this, scope, event)
     if (scope === 'meta') {
+      // console.log('  1----do it!----', instance.scope, instance !== hub)
+      instance.blurf.val = 'haha'
       instance.adapter.val = 3032
-      console.log('ok ok????????', instance.hasOwnProperty('adapter'))
+      // console.log('  2----do it!----', instance.scope, instance.hasOwnProperty('_adapter'),  instance.hasOwnProperty('blurf'))
     }
     return instance
   }
 })
-
-// console.clear()
-
-
-//.catch(console.log)
-
-// setTimeout(() => {
-//   console.log('no!')
-//   hub.set({ text: 'from index' })
-// }, 3500)
