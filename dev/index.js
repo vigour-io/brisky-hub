cfd6928de96a5d138b76770561c9adb9bc33f740
Jim de Beer <jim@vigour.io>
2015-11-07 11:15:46 +0100

    added logs
    Former-commit-id: 70d8ae4a498234c47c2143962df5b7d1c07707b1

--- dev/index.js
+++ dev/index.js
@@ -1,1 +1,4 @@
 'use strict'
+exports.inject = [
+  require('./log/data')
+]
--- example/multiple/client.js
+++ example/multiple/client.js
@@ -1,12 +1,11 @@
 'use strict'
 var uuid = require('vigour-js/lib/util/uuid')
 uuid.val = 'SCOPE_INSTANCE_' + uuid.val
+var Hub = require('../../')
+Hub.prototype.inject(require('../../dev'))
 // Hub.prototype.inject(require('vigour-js/lib/observable/storage'))
 var hub = require('../ui').hub
 hub.adapter.val = {
   scope: 'meta',
   val: 3031
 }
-
-let colors = require('colors-browserify')
-console.log('?xweknweoienwoinde'.blue.bold.rainbow.bgRed, colors)
