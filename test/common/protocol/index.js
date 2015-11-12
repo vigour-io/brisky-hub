e2bbf20b718d9c03e5993c1b70603df51d3fd334
Jim de Beer <jim@vigour.io>
2015-11-12 12:11:08 +0100

    more fixes to protocol
    Former-commit-id: 6e01a69cd7ffb0476b5afddee19583b286133041

--- test/common/protocol/index.js
+++ test/common/protocol/index.js
@@ -18,7 +18,6 @@ describe('hubs', function () {
     a = new Hub({
       key: 'a'
     })
-
     b = new Hub({
       key: 'b'
     })
@@ -27,11 +26,7 @@ describe('hubs', function () {
   it('can set the adapater using a mock protocol', function () {
     a.set({
       adapter: {
-        // multiple protocols! -- first just one
-        // different down then up for example!
         protocol: require('../../../lib/protocol/mock')
-        // down and up in protocol e.g. webrtc! being able to define 2
-        // tcp how to switch when an upstream can recieve tcp - this is the issue
       }
     })
   })
