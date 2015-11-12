a322114c792a4224091cd636c860dda5c005f0c5
Jim de Beer <jim@vigour.io>
2015-11-12 09:56:43 +0100

    added protocol

--- test/common/connection/index.js
+++ test/common/connection/index.js
@@ -1,21 +1,24 @@
 'use strict'
 
-describe('basic connection', function () {
-  var Connection = require('../../../lib/connection')
-  it('can create a connection', function () {
-    var connection = new Connection()
-    console.log('hey hey hey')
-
+describe('protocol', function () {
+  var Protocol = require('../../../lib/protocol')
+  var Connection = require('../../../lib/protocol/connection')
+  it('can create a new connection', function () {
+    var connection = new Connection() // eslint-disable-line
+  })
+  it('can create a new protocol', function () {
+    var connection = new Protocol() // eslint-disable-line
   })
 })
 
 describe('hubs', function () {
-  var a, b
+  var a, b // eslint-disable-line
   var Hub = require('../../../lib')
   it('can create multiple hubs', function () {
     a = new Hub({
       key: 'a'
-    }) // uuid used!
+    })
+
     b = new Hub({
       key: 'b'
     })
