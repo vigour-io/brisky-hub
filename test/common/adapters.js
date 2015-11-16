350ac86317c213670a0d5d84bf138aa0b96d906c
Jim de Beer <jim@vigour.io>
2015-11-16 11:26:40 +0100

    added comments

--- test/common/adapters.js
+++ test/common/adapters.js
@@ -1,5 +1,5 @@
 'use strict'
-
+// make mock injectable, and create new thing by default (can never not be new)
 describe('scopes', function () {
   var Hub = require('../../lib')
   var Mock = require('../../lib/protocol/mock')
@@ -15,6 +15,12 @@ describe('scopes', function () {
   server.set({
     adapter: {
       id: 'server',
+      mock: new Mock()
+    }
+  })
+  server2.set({
+    adapter: {
+      id: 'server2',
       mock: new Mock()
     }
   })
