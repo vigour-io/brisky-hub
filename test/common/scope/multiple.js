fe02c82737cbb757d766380fa8bce86fa47ca7d7
Jim de Beer <jim@vigour.io>
2015-11-16 16:48:07 +0100

    split up clients
    Former-commit-id: 840c83a51a568e8b00af1d2a24cfacea20031aac

--- test/common/scope/multiple.js
+++ test/common/scope/multiple.js
@@ -32,14 +32,11 @@ describe('multiple scopes, clients', function () {
     }
   })
 
-  it('connects to the non-scoped data set of server', function (done) {
-    receiver.adapter.set({
-      mock: 'multiple_server'
-    })
-    receiver.adapter.mock.on('connect', function () {
-      expect(server).to.have.property('clients')
-      done()
-    })
+  scopeReceiver.set({
+    adapter: {
+      id: 'multiple_scope_receiver',
+      mock: new Mock()
+    }
   })
 
   it('connects to the non-scoped data set of server', function (done) {
@@ -50,5 +47,26 @@ describe('multiple scopes, clients', function () {
       expect(server).to.have.property('clients')
       done()
     })
+  })
+
+  it('other client connects to the "myScope" data set of server', function (done) {
+    console.clear()
+    scopeReceiver.adapter.set({
+      mock: 'multiple_server',
+      scope: 'myScope'
+    })
+    scopeReceiver.adapter.mock.on('connect', function () {
+      expect(server).to.have.property('clients')
+      // now make sure that clients are not connected!
+      expect(server).to.have.property('_scopes')
+        .which.has.property('myScope')
+      done()
+    })
+  })
+
+  it('clients field is not inherited over scopes', function () {
+    expect(server.clients).to.not.have.property('multiple_scope_receiver')
+    expect(server._scopes.myScope.clients).to.have.property('multiple_scope_receiver')
+    expect(server._scopes.myScope.clients).to.not.have.property('multiple_receiver')
   })
 })
