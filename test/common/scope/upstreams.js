a635b7aedc74cde0499903d36698c18d7f7b958c
Jim de Beer <jim@vigour.io>
2015-11-17 17:06:53 +0100

    start of tripple upstream scope test
    Former-commit-id: 2420c7ba3d5db531a5224b501634d16cc57bf7ba

--- test/common/scope/upstreams.js
+++ test/common/scope/upstreams.js
@@ -3,70 +3,47 @@
 describe('multiple scopes, clients', function () {
   var Hub = require('../../../lib')
   var Mock = require('../../../lib/protocol/mock')
-  var server = new Hub({
-    key: 'server'
-  })
-  var getScope = Hub.prototype.getScope
+  var a = new Hub({ key: 'server_a' })
+  var b = new Hub({ key: 'server_b' })
+  var c = new Hub({ key: 'server_c' })
   var scopeReceiver = new Hub({ key: 'scopeReceiver' })
   var receiver = new Hub({ key: 'receiver' })
-  server.set({
+  a.set({
     adapter: {
       id: 'scope_upstreams_server_a',
       mock: new Mock()
-    },
-    define: {
-      getScope (scope, event) {
-        console.log('get dat scope!', scope)
-        console.log('use this to test a double upstream as well')
-        return getScope.apply(this, arguments)
-      }
     }
   })
 
-  server.adapter.mock.set({ server: 'multiple_server' })
+  b.set({
+    adapter: {
+      id: 'scope_upstreams_server_b',
+      mock: new Mock()
+    }
+  })
+
+  c.set({
+    adapter: {
+      id: 'scope_upstreams_server_c',
+      mock: new Mock()
+    }
+  })
+
+  a.adapter.mock.set({ server: 'scope_upstreams_server_a' })
+  b.adapter.mock.set({ server: 'scope_upstreams_server_b' })
 
   receiver.set({
     adapter: {
-      id: 'multiple_receiver',
+      id: 'scope_upstreams_receiver',
       mock: new Mock()
     }
   })
 
   scopeReceiver.set({
     adapter: {
-      id: 'multiple_scope_receiver',
+      id: 'scope_upstreams_receiver_scope_b',
       mock: new Mock()
     }
   })
 
-  it('connects to the non-scoped data set of server', function (done) {
-    receiver.adapter.set({
-      mock: 'multiple_server'
-    })
-    receiver.adapter.mock.on('connect', function () {
-      expect(server).to.have.property('clients')
-      done()
-    })
-  })
-
-  it('other client connects to the "myScope" data set of server', function (done) {
-    console.clear()
-    scopeReceiver.adapter.set({
-      mock: 'multiple_server',
-      scope: 'myScope'
-    })
-    scopeReceiver.adapter.mock.on('connect', function () {
-      expect(server).to.have.property('clients')
-      // now make sure that clients are not connected!
-      expect(server).to.have.property('_scopes')
-        .which.has.property('myScope')
-      done()
-    })
-  })
-
-  it('clients field is not inherited over scopes', function () {
-    expect(server.clients).to.not.have.property('multiple_scope_receiver')
-    expect(server._scopes.myScope.clients).to.have.property('multiple_scope_receiver')
-    expect(server._scopes.myScope.clients).to.not.have.property('multiple_receiver')
-  })
 })
