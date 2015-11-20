d749b3199001b404e702f285ca97ddef271ec86c
Jim de Beer <jim@vigour.io>
2015-11-20 09:33:12 +0100

    mock protocol does not fire for instances/contexts (like normal websockets)

--- test/common/scope/connection.js
+++ test/common/scope/connection.js
@@ -6,18 +6,38 @@ describe('multiple upstreams, multiple scopes, multiple clients over single conn
   var Mock = require('../../../lib/protocol/mock')
   var getScope = Hub.prototype.getScope
   // servers
-  var a = new Hub({ key: 'server_a' })
-  var b = new Hub({
+  var a = global.a = new Hub({ key: 'server_a' })
+  var b = global.b = new Hub({
     key: 'server_b',
     define: {
       getScope (val, event) {
         var scope = getScope.apply(this, arguments)
+        console.group()
+        console.log('hey scope!', val, scope.adapter.mock.val, this.adapter.mock.val, this !== scope)
+        // for some reason it does not create a new mock/adapter on scope
+        // allready has the upstream!
+
+        // var dont
+        // if(typeof this.adapter.mock.val !== 'string') {
+          console.log('1'.red.bold, this !== scope, this.adapter === scope.adapter)
+          // dont = true
+        // }
         scope.set({
+          key: 'server_bSCOPE_' + val,
           adapter: {
             mock: 'scope_connection_server_a',
             scope: val
           }
         })
+
+        // if(dont) {
+          console.log('2'.red.bold, this.adapter !== scope.adapter, this.adapter.mock !== scope.adapter.mock)
+          // this is suddenly the same????
+          console.log(this.adapter.mock.val !== scope.adapter.mock.val, this.adapter.mock.val)
+        // }
+
+        console.log('should have fired!-------------\n\n')
+        console.groupEnd()
         return scope
       }
     }
@@ -83,12 +103,12 @@ describe('multiple upstreams, multiple scopes, multiple clients over single conn
   })
 
   it('receiverA2 can connect to b, b._scopes.A2 gets connected to a, shares connection', function (done) {
-    console.clear()
-
-    console.log(receiverA2.adapter.mock.connections === receiverA1.adapter.mock.connections)
-    console.log(require('../../../lib/protocol/mock').prototype.connections)
+    // console.clear()
+    // this test is failing so make it better (connect on server is what has to be tested! hard to do since scope is not there yet)
+    console.log('!!!TEST!!!'.magenta.bold.inverse)
+    // console.log(receiverA2.adapter.mock.connections === receiverA1.adapter.mock.connections)
+    // console.log(require('../../../lib/protocol/mock').prototype.connections)
     // so this goes wrong for some reason the stuff gets added on the prototype!
-
     receiverA2.adapter.set({
       scope: 'a2',
       mock: 'scope_connection_server_b'
@@ -98,14 +118,14 @@ describe('multiple upstreams, multiple scopes, multiple clients over single conn
     })
   })
 
-  it('a2 has scope with correct clients object', function () {
+  xit('a2 has scope with correct clients object', function () {
     expect(a).to.have.property('_scopes')
       .which.has.property('a2')
       .which.has.property('clients')
       .which.has.property('scope_connection_server_b')
   })
 
-  it('a set a field on scope a2', function () {
+  xit('a set a field on scope a2', function () {
     a._scopes.a2.set({
       somefield: true
     })
