98bfdb01fb5e53024dc676715569e3b54987be25
Jim de Beer <jim@vigour.io>
2015-11-23 11:04:34 +0100

    multi scope, multi adapter tests

--- test/common/scope/adapters.js
+++ test/common/scope/adapters.js
@@ -18,22 +18,54 @@ describe('multiple adapters, multiple scopes', function () {
     }
   })
 
+  serverA.adapter.set({
+    mock: {
+      server: 'm_scope_a'
+    }
+  })
+
+  serverB.adapter.set({
+    mock: {
+      server: 'm_scope_b'
+    }
+  })
+
   var receiver = new Hub({
     a: {
       adapter: {
         id: 'm_scope_receiver_a',
-        mock: new Mock()
+        mock: new Mock(),
+        scope: 'james'
       }
     },
     b: {
       adapter: {
         id: 'm_scope_receiver_b',
-        mock: new Mock()
+        mock: new Mock(),
+        scope: 'youzi'
       }
     }
   })
 
-  it('receiver a and b can connect to servers', function () {
+  it('receiver a and b can connect to servers', function (done) {
+    receiver.a.adapter.mock.val = 'm_scope_a'
+    receiver.a.adapter.mock.once('connect', function () {
+      receiver.b.adapter.mock.val = 'm_scope_b'
+      receiver.b.adapter.mock.once('connect', function () {
+        done()
+      })
+    })
+  })
 
+  it('serverA has james scope', function () {
+    expect(serverA._scopes).to.have.property('james')
+      .which.has.property('clients')
+      .which.has.property('m_scope_receiver_a')
+  })
+
+  it('serverB has youzi scope', function () {
+    expect(serverB._scopes).to.have.property('youzi')
+      .which.has.property('clients')
+      .which.has.property('m_scope_receiver_b')
   })
 })
