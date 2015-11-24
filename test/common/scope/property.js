2db7c10b4c279ba39c4f4eab3569593cb327d847
Jim de Beer <jim@vigour.io>
2015-11-24 09:49:48 +0100

    added scope property, easy method to define scopes
    Former-commit-id: c242747f849248bcd3f4d92b3cf4149a0433b491

---
+++ test/common/scope/property.js
@@ -0,0 +1,45 @@
+'use strict'
+
+describe('scopes', function () {
+  var Hub = require('../../../lib')
+  var hub = new Hub()
+  it('can define scope using the scope property, setObject', function () {
+    var james = {
+      aField: true
+    }
+    hub.set({
+      scope: {
+        james: james
+      }
+    })
+    expect(hub._scopeProperties)
+      .to.have.property('james')
+      .which.has.property('_input')
+      .which.equals(james)
+  })
+
+  it('uses james setobject when getting james scope', function () {
+    var jamesScope = hub.getScope('james')
+    expect(jamesScope).to.have.property('aField')
+  })
+
+  it('can define scope using the scope property, function', function (done) {
+    hub.set({
+      scope: {
+        marcus () {
+          done()
+        }
+      }
+    })
+    hub.getScope('marcus')
+  })
+
+  it('can overwrite the getScope function', function (done) {
+    hub.set({
+      scope (scope, event) {
+        done()
+      }
+    })
+    hub.getScope('something')
+  })
+})
