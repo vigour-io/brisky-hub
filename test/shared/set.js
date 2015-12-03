f319d75186556c9a167fd4f03e3dd00aa7aa8d20
Jim de Beer <jim@vigour.io>
2015-12-03 09:36:51 +0100

    set tests work for websockets and mock

--- test/shared/set.js
+++ test/shared/set.js
@@ -7,6 +7,7 @@ module.exports = function (protocol, key) {
     var Observable = require('vigour-js/lib/observable')
     var something = new Observable({ bla: true })
     var Event = require('vigour-js/lib/event')
+    var mock = key === 'mock'
 
     it('can create multiple hubs', function () {
       server = new Hub({
@@ -42,66 +43,86 @@ module.exports = function (protocol, key) {
     it('can create a server "server"', function () {
       server.adapter.set({
         [key]: {
-          server: 'set_server'
+          server: mock ? 'set_server' : 6001
         }
       })
     })
 
     it('receiver can connect to server', function (done) {
-      receiver.adapter.set({ [key]: 'set_server' })
+      receiver.adapter.set({ [key]: mock ? 'set_server' : 'ws://localhost:6001' })
       // maybe make this a bit easier to access e.g. protocol will get a connected observable
       receiver.adapter[key].client.origin.connection.origin.on('connect', done)
     })
 
-    it('receiver can send data to server', function () {
+    it('receiver can send data to server', function (done) {
+      server.once(function () {
+        expect(server).to.have.property('somefield')
+        .which.has.property('_input').equals(true)
+        done()
+      })
       receiver.set({
         somefield: true
       })
-      expect(server).to.have.property('somefield')
-      .which.has.property('_input').equals(true)
     })
 
-    it('server can send data to receiver', function () {
+    it('server can send data to receiver', function (done) {
+      receiver.once(function () {
+        expect(receiver).to.have.property('anotherfield')
+          .which.has.property('_input').equals(true)
+        done()
+      })
       server.set({
         anotherfield: true
       })
-      expect(receiver).to.have.property('anotherfield')
-        .which.has.property('_input').equals(true)
     })
 
-    it('server can send referenced data to receiver', function () {
+    it('server can send referenced data to receiver', function (done) {
+      receiver.once(function () {
+        expect(receiver).to.have.property('a')
+        expect(receiver).to.have.property('referenced')
+          .which.has.property('_input')
+          .which.equals(receiver.a)
+        done()
+      })
       server.set({ a: true }, false)
       server.set({ referenced: server.a })
-      expect(receiver).to.have.property('a')
-      expect(receiver).to.have.property('referenced')
-        .which.has.property('_input')
-        .which.equals(receiver.a)
     })
 
-    it('receiver can send referenced data to server', function () {
+    it('receiver can send referenced data to server', function (done) {
+      server.once(function () {
+        expect(server).to.have.property('field')
+          .which.has.property('_input')
+          .which.equals(server.referenced)
+        done()
+      })
       receiver.set({ field: receiver.referenced })
-      expect(server).to.have.property('field')
-        .which.has.property('_input')
-        .which.equals(server.referenced)
     })
 
-    it('server can send referenced data to receiver, changing a reference', function () {
+    it('server can send referenced data to receiver, changing a reference', function (done) {
+      receiver.field.once(function () {
+        expect(this).to.have.property('_input')
+          .which.equals(receiver.a)
+        done()
+      })
       server.set({ field: server.a })
-      expect(receiver).to.have.property('field')
-        .which.has.property('_input')
-        .which.equals(receiver.a)
     })
 
-    it('server can send out of adapter scope references to receiver', function () {
+    it('server can send out of adapter scope references to receiver', function (done) {
+      receiver.once(function () {
+        expect(receiver).to.have.property('something')
+          .which.has.property('bla')
+          .which.has.property('_input').which.equals(true)
+        done()
+      })
       server.set({ something: something })
-      expect(receiver).to.have.property('something')
-        .which.has.property('bla')
-        .which.has.property('_input').which.equals(true)
     })
 
-    it('server can send out of adapter scope references to receiver, updates from the references value', function () {
+    it('server can send out of adapter scope references to receiver, updates from the references value', function (done) {
+      receiver.something.once(function () {
+        expect(receiver.something).to.have.property('otherfield')
+        done()
+      })
       something.set({ otherfield: true })
-      expect(receiver.something).to.have.property('otherfield')
     })
 
     it('receiver can send custom stamps over the server to another receiver', function (done) {
@@ -113,7 +134,7 @@ module.exports = function (protocol, key) {
       })
 
       receiver2.adapter.set({
-        [key]: 'set_server'
+        [key]: mock ? 'set_server' : 'ws://localhost:6001'
       })
 
       receiver2.adapter[key].once('connect', function () {
