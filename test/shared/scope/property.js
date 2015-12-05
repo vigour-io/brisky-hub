ebee1cc6c00acb4cd8a74328d8f2014e56c8e539
Jim de Beer <jim@vigour.io>
2015-12-05 18:59:37 +0100

    use new as event to check if a cope is created on a server
    Former-commit-id: f24526e2b400078c19dd89099d6cdc5efe23d9a9

--- test/shared/scope/property.js
+++ test/shared/scope/property.js
@@ -1,45 +1,47 @@
 'use strict'
 
-describe('property', function () {
-  var Hub = require('../../../lib')
-  var hub = new Hub()
-  it('can define scope using the scope property, setObject', function () {
-    var james = {
-      aField: true
-    }
-    hub.set({
-      scope: {
-        james: james
+module.exports = function (protocol, key) {
+  describe('property', function () {
+    var Hub = require('../../../lib')
+    var hub = new Hub()
+    it('can define scope using the scope property, setObject', function () {
+      var james = {
+        aField: true
       }
+      hub.set({
+        scope: {
+          james: james
+        }
+      })
+      expect(hub._scopeProperties)
+        .to.have.property('james')
+        .which.has.property('_input')
+        .which.equals(james)
     })
-    expect(hub._scopeProperties)
-      .to.have.property('james')
-      .which.has.property('_input')
-      .which.equals(james)
-  })
 
-  it('uses james setobject when getting james scope', function () {
-    var jamesScope = hub.getScope('james')
-    expect(jamesScope).to.have.property('aField')
-  })
+    it('uses james setobject when getting james scope', function () {
+      var jamesScope = hub.getScope('james')
+      expect(jamesScope).to.have.property('aField')
+    })
 
-  it('can define scope using the scope property, function', function (done) {
-    hub.set({
-      scope: {
-        marcus () {
+    it('can define scope using the scope property, function', function (done) {
+      hub.set({
+        scope: {
+          marcus () {
+            done()
+          }
+        }
+      })
+      hub.getScope('marcus')
+    })
+
+    it('can overwrite the getScope function', function (done) {
+      hub.set({
+        scope (scope, event) {
           done()
         }
-      }
+      })
+      hub.getScope('something')
     })
-    hub.getScope('marcus')
   })
-
-  it('can overwrite the getScope function', function (done) {
-    hub.set({
-      scope (scope, event) {
-        done()
-      }
-    })
-    hub.getScope('something')
-  })
-})
+}
