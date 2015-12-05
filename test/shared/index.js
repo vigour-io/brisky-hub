5cf8ec0f9a10c77dfebf6e9fa193e89280ba2795
Jim de Beer <jim@vigour.io>
2015-12-05 15:50:08 +0100

    kill servers when reapplying them

--- test/shared/index.js
+++ test/shared/index.js
@@ -1,5 +1,8 @@
 'use strict'
 module.exports = function (protocol, key) {
+  require('./constructor.js')
+  require('./protocol')
   require('./set')(protocol, key)
   require('./reconnect')(protocol, key)
+  require('./client')(protocol, key)
 }
