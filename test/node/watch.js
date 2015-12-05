4b8abb42ef313e2133e9fc562d1edb0c9a4fa772
Jim de Beer <jim@vigour.io>
2015-12-05 13:36:14 +0100

    use inherit for watcher (preserve colors)
    Former-commit-id: f2b2ced21fd18ece22a75acfabdd83162997e71a

--- test/node/watch.js
+++ test/node/watch.js
@@ -5,12 +5,8 @@ try {
 }
 
 var spawn = require('child_process').spawn
-var gaston = spawn('gaston', ['test', '-r', 'node', '-s', process.cwd() + '/test/node/index.js'])
-
-gaston.stdout.on('data', function (data) {
-  process.stdout.write(data.toString())
-})
-
-gaston.on('close', function (code, signal) {
-  console.log(code, signal)
-})
+var gaston = spawn( //eslint-disable-line
+  'gaston',
+  ['test', '-r', 'node', '-s', process.cwd() + '/test/node/index.js'],
+  { stdio: 'inherit' }
+)
