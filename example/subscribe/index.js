547abe6fccb859708d40049b6368f2b6486fd74b
Jim de Beer <jim@vigour.io>
2015-12-14 15:01:41 +0100

    added subscription in hub
    Former-commit-id: beed22b5ca3c9d69b3e73d6cb945b6aff629b41c

--- example/subscribe/index.js
+++ example/subscribe/index.js
@@ -4,34 +4,51 @@ var Observable = require('vigour-js/lib/observable')
 var colors = require('colors-browserify')
 
 var Hub = require('../../lib')
-var hub = new Hub({
-  adapter: {
-    inject: require('../../lib/protocol/websocket'),
-    val: 'ws://localhost:3031'
-  },
-  time: true
-})
 
-var a = new Observable({
-  b: true,
-  time: {
-    on: {
-      data () {}
+var server = global.server = new Hub({
+  adapter: {
+    id: 'randomserver',
+    inject: require('../../lib/protocol/mock/'),
+    mock: {
+      server: 'testserver'
     }
   }
 })
 
-hub.subscribe({
+server.get('time', {})
+
+var client = global.client = new Hub({
+  adapter: {
+    id: 'testclient',
+    inject: require('../../lib/protocol/mock'),
+    // val: 'ws://localhost:3031'
+    mock: 'testserver'
+  }
+})
+
+client.get('time', {})
+
+client.adapter.mock.connected.is(true, function () {
+  console.log('lullllzzzzzz'.rainbow)
+  setTimeout(() => {
+    server.set({
+      time: 222
+    })
+  }, 100)
+})
+
+client.subscribe({
   time: true
 }, function (data, event) {
   for (var i in data) {
     // here we want the real data!
     console.log('hello', data[i].origin.path.join('.'), data[i].data, event)
-
     // console.log(data[i].origin._on.data.getBound(event, this).data)
   }
 })
 
-hub.set({
-  time: 222
+client.time.on(function (data) {
+  console.log('mystery ballz', data)
 })
+
+client.set({flurps: true})
