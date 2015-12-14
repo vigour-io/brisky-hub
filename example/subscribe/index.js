cbb69c6942984193cc6f479fec49750f47d25689
Jim de Beer <jim@vigour.io>
2015-12-14 14:07:22 +0100

    start of subscription

--- example/subscribe/index.js
+++ example/subscribe/index.js
@@ -1,10 +1,37 @@
+'use strict'
 var Observable = require('vigour-js/lib/observable')
+// Observable.prototype.inject(require('vigour-js/li'))
+var colors = require('colors-browserify')
+
 var Hub = require('../../lib')
-console.log('starting...')
-console.time('test1')
-var obj = require('../data.json')
-console.timeEnd('test1')
-console.time('test2')
-var hub = new Observable(obj)
-console.timeEnd('test2')
-console.log('done')
+var hub = new Hub({
+  adapter: {
+    inject: require('../../lib/protocol/websocket'),
+    val: 'ws://localhost:3031'
+  },
+  time: true
+})
+
+var a = new Observable({
+  b: true,
+  time: {
+    on: {
+      data () {}
+    }
+  }
+})
+
+hub.subscribe({
+  time: true
+}, function (data, event) {
+  for (var i in data) {
+    // here we want the real data!
+    console.log('hello', data[i].origin.path.join('.'), data[i].data, event)
+
+    // console.log(data[i].origin._on.data.getBound(event, this).data)
+  }
+})
+
+hub.set({
+  time: 222
+})
