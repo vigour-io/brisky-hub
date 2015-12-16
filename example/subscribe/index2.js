bb524602527b4172c6d62c3e41c5c2390ea93269
Jim de Beer <jim@vigour.io>
2015-12-16 15:08:32 +0100

    subscribe overwrite
    Former-commit-id: 8e7c1483dea025876433d6efa65e9911fd324f06

---
+++ example/subscribe/index2.js
@@ -0,0 +1,123 @@
+'use strict'
+// var Observable = require('vigour-js/lib/observable')
+var colors = require('colors-browserify') //eslint-disable-line
+var Hub = require('../../lib')
+require('./style.less')
+console.line = false
+
+var client = global.client = new Hub({
+  key: 'client',
+  adapter: {
+    inject: require('../../lib/protocol/websocket'),
+    websocket: 'ws://localhost:3032'
+  }
+})
+
+client.get('shows', {})
+
+var App = require('vigour-element/lib/app')
+
+var Element = require('vigour-element')
+Element.prototype.inject(
+  require('vigour-element/lib/property/text'),
+  require('vigour-element/lib/property/css'),
+  require('vigour-element/lib/events'),
+  require('vigour-element/lib/property/transform'),
+  require('vigour-element/lib/events/drag')
+)
+
+var app = new App({
+  key: 'app',
+  node: document.body,
+  // btn: {
+  //   node: 'button',
+  //   text: 'ok lets unsubscribe',
+  //   on: {
+  //     click () {
+  //       console.clear()
+  //       if (app._input !== client) {
+  //         app.val = client
+  //         this.text.val = 'ok lets unsubscribe'
+  //       } else {
+  //         app.val = false
+  //         this.text.val = 'ok lets subscribe'
+  //       }
+  //       // obs.val = obs._input === client ? 'haha' : client
+  //     }
+  //   }
+  // },
+  // removebtn: {
+  //   node: 'button',
+  //   text: 'REMOVE',
+  //   on: {
+  //     click () {
+  //       console.clear()
+  //       if (this.parent.textfield) {
+  //         this.parent.textfield.remove()
+  //       } else {
+  //         this.parent.set({textfield:{ text: { $: 'mybitch' }}})
+  //       }
+  //     }
+  //   }
+  // },
+  // textfield: {
+  //   css: 'thing',
+  //   node: 'input',
+  //   text: {
+  //     inject: require('vigour-js/lib/operator/subscribe'),
+  //     $: 'mybitch'
+  //   },
+  //   on: {
+  //     input () {
+  //       this.text.origin.val = this.node.value
+  //     }
+  //   }
+  // },
+  addBtn: {
+    node: 'button',
+    text: 'THIS IS 3032!!! add something',
+    on: {
+      click () {
+        this.parent.collection.origin.set({
+          [Math.random()]: {
+            title: Math.random()
+          }
+        })
+      }
+    }
+  },
+  collection: {
+    text: 'collection',
+    inject: require('vigour-js/lib/operator/subscribe'),
+    ChildConstructor: new Element({
+      css: 'thing',
+      node: 'input',
+      text: {
+        inject: require('vigour-js/lib/operator/subscribe'),
+        $: 'title'
+      },
+      on: {
+        input () {
+          var v = this.node.value
+          if (!v) {
+            this.origin.remove()
+          } else {
+            this.text.origin.val = this.node.value
+          }
+        }
+      }
+    }),
+    $: 'shows'
+  },
+  val: client
+})
+
+app.subscribe({
+  $upward: {
+    shows: {
+      $any: {
+        title: true
+      }
+    }
+  }
+})
--- example/subscribe/server2.js
+++ example/subscribe/server2.js
@@ -2,14 +2,14 @@ var Hub = require('../../lib')
 
 var hub = new Hub({ //eslint-disable-line
   adapter: {
+    id: 'funtimes2',
     inject: require('../../lib/protocol/websocket'),
-    id: 'myown',
     websocket: {
-      val: 'ws://localhost:3032',
-      server: 3031
+      server: 3032
+      // val: 'ws://localhost:3033'
     }
   }
 })
 
 require('colors-browserify')
-console.log('server 1 start:', hub.adapter.id.rainbow)
+console.log('server 2 start 3032:', hub.adapter.id.rainbow)
