d4e88269ff61162a53b9462fa852023c00a6ba13
Jim de Beer <jim@vigour.io>
2015-12-17 21:15:38 +0100

    added todo-todo raggen

--- example/subscribe/todoraggen.js
+++ example/subscribe/todoraggen.js
@@ -1,5 +1,5 @@
 'use strict'
-var Observable = require('vigour-js/lib/observable') //eslint-disable-line
+var Observable = require('vigour-js/lib/observable')
 var colors = require('colors-browserify') //eslint-disable-line
 var Hub = require('../../lib')
 require('./style.less')
@@ -170,4 +170,4 @@ var app = new App({ //eslint-disable-line
   val: client
 })
 
-// a --- b -- multiple instances b --- fire all instances / contexts of b!!!!
+//a --- b -- multiple instances b --- fire all instances / contexts of b!!!!
