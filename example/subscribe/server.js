c213b3e68c42c7a74e1739ab9018d18f4aa79d8c
Jim de Beer <jim@vigour.io>
2015-12-21 21:05:03 +0100

    added transformer
    Former-commit-id: 9d1daece457ebd2d406061310c5f409c8862e046

--- example/subscribe/server.js
+++ example/subscribe/server.js
@@ -1,18 +1,39 @@
+require('colors-browserify')
 var Hub = require('../../lib')
-
+var http = require('http')
+var JSONStream = require('JSONStream')
 var hub = new Hub({ //eslint-disable-line
   adapter: {
     inject: require('../../lib/protocol/websocket'),
-    id: 'myown',
+    id: 'mtv',
     websocket: {
-      server: 3031
+      server: 3031,
+      val: 'ws://localhost:3032'
     }
   },
-  levelup: 'myown',
+  levelup: 'mtv',
   shows: {
     a: true
   }
 })
 
-require('colors-browserify')
-console.log('server 1 start:', hub.adapter.id.rainbow)
+hub.levelready.is(true, function () {
+  if (!hub.datafromjson) {
+    console.log('LOAD DATA EXTERNAL!'.magenta)
+    http.request({
+      host: 'scraper.dev.vigour.io', // 'scraper-de-staging.dev.vigour.io',
+      path: '/new.json',
+      port: 80,
+      method: 'get',
+      headers: {
+        accepts: '*/*'
+      }
+    }, function (res) {
+      console.log('xxxx')
+      res.pipe(JSONStream.parse('mtvData.*.*'))
+      .on('end', function () {
+        hub.setKey('datafromjson', true)
+      })
+    }).end()
+  }
+})
