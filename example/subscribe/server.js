7b8af086dadc8a69462cdee01777ae84d5e54465
Jim de Beer <jim@vigour.io>
2015-12-21 21:24:05 +0100

    show chunks

--- example/subscribe/server.js
+++ example/subscribe/server.js
@@ -11,6 +11,7 @@ var hub = new Hub({ //eslint-disable-line
       val: 'ws://localhost:3032'
     }
   },
+  shows: {},
   levelup: 'mtv'
   // datafromjson: false
 })
@@ -19,7 +20,7 @@ hub.levelready.is(true, function () {
   if (!hub.datafromjson || hub.datafromjson.val !== true) {
     console.log('start loading!'.magenta)
     http.request({
-      host:  'scraper.dev.vigour.io', //'scraper-de-staging.dev.vigour.io',
+      host: 'scraper.dev.vigour.io',
       path: '/new.json',
       port: 80,
       method: 'get',
@@ -27,14 +28,15 @@ hub.levelready.is(true, function () {
         accepts: '*/*'
       }
     }, function (res) {
-      res.pipe(JSONStream.parse('mtvData.*.*'))
+      res.pipe(JSONStream.parse('mtvData.*.*.shows.*'))
       .on('data', function (data) {
-        console.log('CHUNK!'.magenta)
-        // make this atomic wat to heavy
-        hub.set(data)
+        if (data.id) {
+          hub.shows.setKey(data.id , data)
+        }
       })
       .on('end', function () {
         hub.setKey('datafromjson', true)
+        console.log('LOADED!'.magenta)
       })
     }).end()
   }
