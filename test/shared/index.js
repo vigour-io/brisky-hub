1b45c409bac7f8b49534fcec3c4654742768633a
Jim de Beer <jim@vigour.io>
2015-12-23 22:18:17 +0100

    cleanup tests

--- test/shared/index.js
+++ test/shared/index.js
@@ -1,12 +1,12 @@
 'use strict'
 module.exports = function (protocol, key) {
-  require('./constructor')
-  require('./protocol')
-  require('./set')(protocol, key)
-  require('./references')(protocol, key)
-  require('./remove')(protocol, key)
-  require('./reconnect')(protocol, key)
-  require('./client')(protocol, key)
-  require('./adapters')(protocol, key)
+  // require('./constructor')
+  // require('./protocol')
+  // require('./set')(protocol, key)
+  // require('./references')(protocol, key)
+  // require('./remove')(protocol, key)
+  // require('./reconnect')(protocol, key)
+  // require('./client')(protocol, key)
+  // require('./adapters')(protocol, key)
   require('./scope')(protocol, key)
 }
