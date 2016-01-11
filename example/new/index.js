40daf02a2687de29d537e7b673d9947cac3bc37b
Jim de Beer <jim@vigour.io>
2016-01-11 14:43:11 +0100

    update
    Former-commit-id: 6532b9b4ec290f51c2a7c6bb6bc931de401960fd

--- example/new/index.js
+++ example/new/index.js
@@ -3,11 +3,24 @@ require('./style.less')
 var Hub = require('../../lib')
 var hub = global.hub = new Hub()
 
-hub.set({
-  adapter: {
-    inject: require('../../lib/protocol/websocket'),
-    websocket: 'ws://localhost:3031'
+// hub.set({
+//   adapter: {
+//     inject: require('../../lib/protocol/websocket'),
+//     websocket: 'ws://localhost:3031'
+//   }
+// })
+
+var Observable = require('vigour-js/lib/observable')
+ var a = new Observable({
+  text: {
+    on: {
+      data (data, event) {
+        console.log(data, event)
+      }
+    }
   }
 })
 
+console.clear()
+a.text.val = 'xxxxx'
 // why not why not o why not!
