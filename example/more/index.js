069cc213198b33581dbf0edfd7b5f574a6abb5a2
Jim de Beer <jim@vigour.io>
2016-01-12 23:33:01 +0100

    update

--- example/more/index.js
+++ example/more/index.js
@@ -7,7 +7,7 @@ require('./style.less')
 hub.set({
   adapter: {
     inject: require('../../lib/protocol/websocket'),
-    websocket: 'ws://localhost:50500'
+    websocket: 'ws://localhost:3031'
   },
   shows: {
     // 977: {
