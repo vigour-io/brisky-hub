686bef39878f40b0124bea4bddf5c2a606f5cdb8
Jim de Beer <jim@vigour.io>
2016-01-13 19:04:42 +0100

    update

--- example/more/server.js
+++ example/more/server.js
@@ -114,6 +114,7 @@ if (!hub.datafromjson || hub.datafromjson.val !== true) {
           hub.shows[data.id].seasons.each((p) => {
             p.episodes.each((p) => {
               p.set({ time: Math.random() })
+              p.set({ video: p.mrss.val })
             })
           })
           // getImgBase64(hub.shows[data.id])
