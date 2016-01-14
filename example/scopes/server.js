5ad1acf70665fab24172501e65984d24f41a6664
Jim de Beer <jim@vigour.io>
2016-01-14 20:14:30 +0100

    update
    Former-commit-id: e17ab63aa9a852ef8111d16bbae9b4ca1935eed4

--- example/scopes/server.js
+++ example/scopes/server.js
@@ -1,6 +1,9 @@
 process.stdout.write('\033c') //eslint-disableore-line
 console.log('start scope!')
 'use strict'
+
+const repl = require('repl')
+
 var Hub = require('../../lib')
 var fs = require('fs')
 // var colors = require('colors-browserify')
@@ -20,4 +23,39 @@ var hub = new Hub({ //eslint-disable-line
 
 hub.adapter.websocket.set({
   server: 3031
+})
+
+repl.start('> ').context.hub = hub
+
+
+hub.define({
+  tf: {
+    get () {
+      var tf = this.textfield
+      return this.textfield._on.data.attach
+    }
+  },
+  t () {
+    var tf = this.textfield
+
+    if (tf) {
+      var b = tf._on
+
+      if(b) {
+        var c = b.data && b.data.attach
+      }
+      for (var i in b.data.attach) {
+        if (i !== 'key' && b.data.attach[i] instanceof Array) {
+          var client = b.data.attach[i][1]
+
+          console.log(i, client.val, client.lookUp('_scope'), this._scope)
+          // client
+          // return client
+        }
+      }
+    }
+  },
+  g () {
+    console.log(this._scopes.anon.textfield instanceof this.textfield.Constructor)
+  }
 })
