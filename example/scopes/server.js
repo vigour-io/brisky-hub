4c0b171f2b1ccfcbac48d7b9280c7cac6b69fdd8
Jim de Beer <jim@vigour.io>
2016-01-14 14:06:15 +0100

    update

---
+++ example/scopes/server.js
@@ -0,0 +1,25 @@
+process.stdout.write('\033c') //eslint-disableore-line
+console.log('start scope!')
+'use strict'
+var Hub = require('../../lib')
+var fs = require('fs')
+// var colors = require('colors-browserify')
+// var http = require('http')
+// var JSONStream = require('JSONStream')
+var hub = new Hub({ //eslint-disable-line
+  adapter: {
+    inject: require('../../lib/protocol/websocket'),
+    id: 'mtv',
+    websocket: {}
+  },
+  scopes () {
+    console.log('lulzzzz scope!', arguments)
+  },
+  afield: {
+    text: 'lulz'
+  }
+})
+
+hub.adapter.websocket.set({
+  server: 3031
+})
