a0e12dd3ecaaea6b856fcab43739f4f311ef4466
Youri Daamen <me@youz.io>
2016-01-15 09:02:57 +0100

    real data
    Former-commit-id: 72696f749fa542e438bd6d3645cee3baeb37008a

--- example/more/server.js
+++ example/more/server.js
@@ -3,7 +3,7 @@ console.log('start!')
 'use strict'
 var Hub = require('../../lib')
 var fs = require('fs')
-// var colors = require('colors-browserify')
+  // var colors = require('colors-browserify')
 var http = require('http')
 var JSONStream = require('JSONStream')
 var hub = new Hub({ //eslint-disable-line
@@ -22,7 +22,7 @@ var hub = new Hub({ //eslint-disable-line
       free: {},
       recommended: {},
       popular: {},
-      new:{}
+      new: {}
     }
   },
   channels: {}
@@ -32,22 +32,22 @@ var hub = new Hub({ //eslint-disable-line
 
 var https = require('https')
 
-function getImgBase64 (show) {
+function getImgBase64(show) {
   getImgBase64nest(show)
-  // ff wat beter concurrency handlen
-  // show.seasons.each((p) => {
-  //   p.episodes.each(function (pr) {
-  //     getImgBase64nest(pr)
-  //   })
-  // })
+    // ff wat beter concurrency handlen
+    // show.seasons.each((p) => {
+    //   p.episodes.each(function (pr) {
+    //     getImgBase64nest(pr)
+    //   })
+    // })
 }
 
-function getImgBase64nest (show) {
+function getImgBase64nest(show) {
   // console.log('lezzgo!', show.img && show.img.val)
-  if (!show.img|| !show.img.val) {
+  if (!show.img || !show.img.val) {
     return
   }
-   https.request({
+  https.request({
     host: 'imgmtvplay-a.akamaihd.net',
     path: '/image/4/4?url=http://images.mtvnn.com/' + show.img.val + '/original',
     port: 443,
@@ -59,11 +59,13 @@ function getImgBase64nest (show) {
     var str = ''
     res.on('data', function (data) {
       str += data.toString('base64')
-      // console.log('yo lil data', arguments)
+        // console.log('yo lil data', arguments)
     })
     res.on('end', function (err, data) {
       if (!err && str) {
-        show.set({ thumb: str })
+        show.set({
+          thumb: str
+        })
       }
     })
   }).end()
@@ -78,9 +80,9 @@ if (!hub.datafromjson || hub.datafromjson.val !== true) {
     var count = 0
 
     http.request({
-      host: 'scraper.dev.vigour.io',
+      host: '37.48.93.74',
       path: '/new.json',
-      port: 80,
+      port: 51004,
       method: 'get',
       headers: {
         accepts: '*/*'
@@ -88,68 +90,129 @@ if (!hub.datafromjson || hub.datafromjson.val !== true) {
     }, function (res) {
       // load channels
       res.pipe(JSONStream.parse('mtvData.*.*.channels.*'))
-      .on('data', function (data) {
-        if (data.id) {
-          // console.log('channel from json:', data.id, data)
-          hub.channels.set({ [data.id]: data })
-          hub.channels[data.id].setKey('currentEpisode', hub.channels[data.id])
-        } else {
-          // console.log('channel from json --> no id:'.red, data)
-        }
-      })
+        .on('data', function (data) {
+          if (data.id) {
+            // console.log('channel from json:', data.id, data)
+            hub.channels.set({
+              [data.id]: data
+            })
+            hub.channels[data.id].setKey('currentEpisode', hub.channels[data.id])
+          } else {
+            // console.log('channel from json --> no id:'.red, data)
+          }
+        })
 
       // load shows
       res.pipe(JSONStream.parse('mtvData.*.*.shows.*'))
-      .on('data', function (data) {
-        if (data.id) {
-          console.log('show from json:', data.id, data.img)
-          // event ofc
+        .on('data', function (data) {
+          if (data.id) {
+            console.log('show from json:', data.id, data.img)
+              // event ofc
 
-          hub.shows.set({ [data.id]: data })
-
-          hub.shows[data.id].set({
-            currentEpisode: hub.shows[data.id].seasons[0].episodes[0],
-            currentSeason: hub.shows[data.id].seasons[0]
-          })
-
-          hub.shows[data.id].seasons.each((p) => {
-            p.episodes.each((p) => {
-              p.set({ time: Math.random() })
-              p.set({ video: p.mrss.val })
-              // if(p.mrss.val === 'c5cc5ef90b81d94e3fb0') {
-              //   console.log(p.title.val)
-              //   var MRSS =
-              // }
+            hub.shows.set({
+              [data.id]: data
             })
-          })
-          // getImgBase64(hub.shows[data.id])
 
-          count++
-          if (count <= 3) {
-            hub.discover.carousel.set({
-              [data.id]: hub.shows[data.id]
+            hub.shows[data.id].set({
+              currentEpisode: hub.shows[data.id].seasons[0].episodes[0],
+              currentSeason: hub.shows[data.id].seasons[0]
             })
-          } else if (count <= 10) {
+
+            hub.shows[data.id].seasons.each((p) => {
+                p.episodes.each((p) => {
+                  p.set({
+                    time: Math.random()
+                  })
+                  p.set({
+                      video: p.mrss.val
+                    })
+                    // if(p.mrss.val === 'c5cc5ef90b81d94e3fb0') {
+                    //   console.log(p.title.val)
+                    //   var MRSS =
+                    // }
+                })
+              })
+              // getImgBase64(hub.shows[data.id])
+
+            // count++
+            // if (count <= 3) {
+            //   hub.discover.carousel.set({
+            //     [data.id]: hub.shows[data.id]
+            //   })
+            // } else if (count <= 10) {
+            //   hub.discover.lists.free.set({
+            //     [data.id]: hub.shows[data.id]
+            //   })
+            // } else if (count <= 15) {
+            //   hub.discover.lists.recommended.set({
+            //     [data.id]: hub.shows[data.id]
+            //   })
+            // } else if (count <= 20) {
+            //   hub.discover.lists.popular.set({
+            //     [data.id]: hub.shows[data.id]
+            //   })
+            // } else if (count <= 25) {
+            //   hub.discover.lists.new.set({
+            //     [data.id]: hub.shows[data.id]
+            //   })
+            // }
+          } else {
+            // console.log('show from json --> no id:', data)
+          }
+        })
+        // load discover
+      res.pipe(JSONStream.parse('mtvData.*.*.discover_marquee_free.list.*'))
+        .on('data', function (data) {
+          // show or channel etc
+          var link = data.link && data.link.split('.')
+          if (link) {
+            var length = link.length
+            var id = link[length - 1]
             hub.discover.lists.free.set({
-              [data.id]: hub.shows[data.id]
-            })
-          } else if (count <= 15) {
-            hub.discover.lists.recommended.set({
-              [data.id]: hub.shows[data.id]
-            })
-          } else if (count <= 20) {
-            hub.discover.lists.popular.set({
-              [data.id]: hub.shows[data.id]
-            })
-          } else if (count <= 25) {
-            hub.discover.lists.new.set({
-              [data.id]: hub.shows[data.id]
+              [id]: hub.get(link.slice(3, length), {})
             })
           }
-        } else {
-          // console.log('show from json --> no id:', data)
-        }
-      })
+        })
+
+      res.pipe(JSONStream.parse('mtvData.*.*.discover_row1_free.list.*'))
+        .on('data', function (data) {
+          // show or channel etc
+          var link = data.link && data.link.split('.')
+          if (link) {
+            var length = link.length
+            var id = link[length - 1]
+            hub.discover.lists.popular.set({
+              [id]: hub.get(link.slice(3, length), {})
+            })
+          }
+        })
+
+      res.pipe(JSONStream.parse('mtvData.*.*.discover_row2_free.list.*'))
+        .on('data', function (data) {
+          // show or channel etc
+          var link = data.link && data.link.split('.')
+          if (link) {
+            var length = link.length
+            var id = link[length - 1]
+            hub.discover.lists.recommended.set({
+              [id]: hub.get(link.slice(3, length), {})
+            })
+          }
+        })
+
+      res.pipe(JSONStream.parse('mtvData.*.*.discover_row3_free.list.*'))
+        .on('data', function (data) {
+          // show or channel etc
+          var link = data.link && data.link.split('.')
+          if (link) {
+            var length = link.length
+            var id = link[length - 1]
+            hub.discover.lists.new.set({
+              [id]: hub.get(link.slice(3, length), {})
+            })
+          }
+        })
+
       .on('end', function () {
         console.log('LOADED!')
 
