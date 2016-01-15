7d9499bc8d07134bdee07009c97ba4e11989ec86
Jim de Beer <jim@vigour.io>
2016-01-15 02:26:58 +0100

    added simple example (broken)
    Former-commit-id: 033a95cd4a27a925cd8aa4d41c0b2dbd689e0338

---
+++ example/scopes/simple.js
@@ -0,0 +1,36 @@
+'use strict'
+var Hub = require('../../lib')
+var hub = global.hub = new Hub({ //eslint-disable-line
+  adapter: {
+    inject: require('../../lib/protocol/websocket'),
+    // websocket: {}
+    // textfield: 'nothing yet'
+  }
+})
+
+// hub.adapter.scope.val = 'anon'
+hub.adapter.set({ websocket: 'ws://localhost:3031' })
+
+var Element = require('vigour-element')
+var app = require('vigour-element/lib/app')
+
+app.set({
+  holder: {
+    $: true,
+    textfield: {
+      type: 'input',
+      value: {
+        $: 'shows.977.currentSeason.number'
+      },
+      on: {
+        keyup (data, event) {
+          this.value.origin.val = this.node.value
+        }
+      }
+    }
+  }
+})
+
+app.holder.val = hub
+// for the multi-upstreams multi-scope we do need to apply subscriptions to the original -- pretty annoying
+// other fix just add extension capability for this all subs on scopes also go to original: true
