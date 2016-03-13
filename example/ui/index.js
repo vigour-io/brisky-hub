b4b9afa09b3bc359b8ea8ff0b54818bc4042368c
Jim de Beer <jim@vigour.io>
2016-03-13 16:55:55 +0100

    first example --- will be refactored to test
    Former-commit-id: fec16e5238cd4b779494f9c5d566982f6b39a89d

--- example/ui/index.js
+++ example/ui/index.js
@@ -1,8 +1,29 @@
 'use strict'
 var e = require('vigour-element/e')
+var isNode = require('vigour-util/is/node')
 var Hub = require('../../')
-
-e({
-  DOM: document.body,
-  text: 'lulllllz'
+var app
+var hub = new Hub({
+  adapter: {
+    inject: require('../../lib/protocol/websocket'),
+    websocket: {
+      val: 'ws://localhost:3333',
+      connected: {
+        on: {
+          data () {
+            console.log(this, app.indicator.text.val)
+          }
+        }
+      }
+    }
+  }
 })
+
+app = e({
+  DOM: isNode ? {} : document.body,
+  indicator: {
+    text: hub.adapter.websocket.connected
+  }
+})
+
+console.log('lets go!')
--- example/ui/server.js
+++ example/ui/server.js
@@ -1,6 +1,13 @@
 'use strict'
 var Hub = require('../../')
-
 var hub = new Hub({
-
+  adapter: {
+    inject: require('../../lib/protocol/websocket'),
+    websocket: {
+      server: 3333
+    }
+  }
 })
+
+console.log('do we have a server hur?', hub)
+
