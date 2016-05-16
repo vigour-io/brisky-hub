92cfa52ef769d7d1fe18bf84db5704fd95813a7a
Jim de Beer <jim@vigour.io>
2016-05-16 21:26:16 +0200

    connection

--- test/basic.js
+++ test/basic.js
@@ -4,9 +4,10 @@ const Hub = require('../')
 
 test('basic setup', function (t) {
   console.log('start')
-  const server = Hub({ listen: 6060 })
+  const server = Hub({ listen: 6060 }) //eslint-disable-line
   const client = Hub({ connect: 'ws://localhost:6060' })
-  // const client2 = Hub({ connect: 'ws://localhost:6060' })
+  const client2 = Hub({ connect: 'ws://localhost:6060' })
+
   client.subscribe({
     movies: {
       $any: { title: { val: true } }
@@ -14,15 +15,28 @@ test('basic setup', function (t) {
   }, (state, type) => {
     console.log('client RECEIVE DATA', type, state.path())
   })
-  client.connected.once(() => {
-    console.log('lullz connected')
-    server.set({
-      movies: {
-        blargh: {
-          title: 'the blargh'
-        }
+
+  client2.subscribe({
+    movies: {
+      $any: { title: { val: true } }
+    }
+  }, (state, type) => {
+    console.log('client2 RECEIVE DATA', type, state.path())
+  })
+
+  client.set({
+    movies: {
+      blargh: {
+        title: 'the blargh'
       }
-    })
+    }
+  })
+
+  client.connected.once(() => {
+    console.log('lullz client 1 connected')
+  })
+  client2.connected.once(() => {
+    console.log('lullz client 2 connected')
   })
   t.end()
 })
