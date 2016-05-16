aa31a32b3d0cdafc4c7c27c5d161cc3cfb4859f7
Jim de Beer <jim@vigour.io>
2016-05-16 21:03:45 +0200

    basic connection

--- test/basic.js
+++ test/basic.js
@@ -6,6 +6,23 @@ test('basic setup', function (t) {
   console.log('start')
   const server = Hub({ listen: 6060 })
   const client = Hub({ connect: 'ws://localhost:6060' })
-  console.log(server, client)
+  // const client2 = Hub({ connect: 'ws://localhost:6060' })
+  client.subscribe({
+    movies: {
+      $any: { title: { val: true } }
+    }
+  }, (state, type) => {
+    console.log('client RECEIVE DATA', type, state.path())
+  })
+  client.connected.once(() => {
+    console.log('lullz connected')
+    server.set({
+      movies: {
+        blargh: {
+          title: 'the blargh'
+        }
+      }
+    })
+  })
   t.end()
 })
