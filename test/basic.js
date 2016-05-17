46bca2c77afa72ec13b91bdedfb2353e4b938c7c
Jim de Beer <jim@vigour.io>
2016-05-17 16:50:43 +0200

    hub structure, added correct stamp ids

--- test/basic.js
+++ test/basic.js
@@ -5,7 +5,22 @@ const Hub = require('../')
 test('basic setup', function (t) {
   console.log('start')
   // find free port
-  const server = new Hub({ port: 3030 })
+  const server = new Hub({ port: 3030 }) //eslint-disable-line
   const client = new Hub({ url: 'ws://localhost:3030' })
+  client.set({
+    on: {
+      data: {
+        upstream: null // so this does not work...
+      }
+    }
+  })
+  client.subscribe({
+    $any: { val: true }
+  }, (state, type) => {
+    console.log('yo!', type, state.path())
+  })
+
+  console.log('set blurf')
+  client.set({ blurf: true })
   t.end()
 })
