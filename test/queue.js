3d36cc58ec92260e317c85c7b1e558edb33750ff
Jim de Beer <jim@vigour.io>
2016-07-26 14:44:03 +0300

    added more tests

--- test/queue.js
+++ test/queue.js
@@ -14,35 +14,59 @@ test('queue', function (t) {
     id: 1
   })
 
+  client1.subscribe({ $any: { val: true } })
+
   const client2 = new Hub({
     url: 'ws://localhost:6000',
     context: 'blurf',
     id: 2
   })
 
-  client1.set({ a: 1, b: 1 })
-  client2.set({ a: 2, b: 2 })
+  client2.subscribe({ $any: { val: true } })
 
-  server.get('a', {}).on(function (val, stamp) {
-    console.log('incoming', val, stamp, this.parent.context)
-  })
+  client1.set({ a: 1, b: 1, c: 1 })
+  client2.set({ a: 2, b: 2, c: 2 })
 
   isConnected(true, () => {
     const context = server.getContext('blurf')
     t.equal(context.a.val, 2, '"a" is set by "client2"')
     t.equal(context.b.val, 2, '"b" is set by "client2"')
+    t.equal(context.c.val, 2, '"c" is set by "client2"')
     server.port.set(false)
     isConnected(false, disconnect)
   })
 
   function disconnect () {
     t.ok(true, 'disconnected clients')
-    server.port.set(6000)
-    isConnected(true, reconnect)
+    client1.a.set(-1)
+    client1.b.set(-1)
+    setTimeout(() => {
+      client2.a.set(-2)
+      client2.b.set(-2)
+      setTimeout(() => {
+        client1.a.set('a')
+        client1.c.remove()
+        server.port.set(6000)
+        isConnected(true, reconnect)
+      }, 1)
+    }, 1)
   }
 
   function reconnect () {
     t.ok(true, 'reconnected clients')
+    const context = server.getContext('blurf')
+    t.equal(context.a.val, 'a', 'server - "a" is set to client1')
+    t.equal(context.b.val, -2, 'server - "b" is set to client2')
+    t.equal(context.c, null, 'server - "c" is removed')
+
+    t.equal(client1.a.val, 'a', 'client1 - got "a" from client1')
+    t.equal(client1.b.val, -2, 'client1 - got "b" from client2')
+    t.equal(client1.c, null, 'client1 - "c" is removed')
+
+    t.equal(client2.a.val, 'a', 'client2 - got "a" from client1')
+    t.equal(client2.b.val, -2, 'client2 - got "b" from client2')
+    t.equal(client2.c, null, 'client2 - "c" is removed')
+
     server.remove()
     client1.remove()
     client2.remove()
@@ -55,7 +79,7 @@ test('queue', function (t) {
       client1.connected.is(val),
       client2.connected.is(val)
     ]).then(() => {
-      setTimeout(cb, 10)
+      setTimeout(cb, 25)
     }).catch(error)
   }
 })
