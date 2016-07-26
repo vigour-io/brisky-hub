f005eba940e0cbac68455d1363d28777b33b6ec9
Jim de Beer <jim@vigour.io>
2016-07-26 13:57:05 +0300

    first tests -- server close works again

--- test/queue.js
+++ test/queue.js
@@ -1,66 +1,61 @@
 'use strict'
 const test = require('tape')
 const Hub = require('../')
-const vstamp = require('vigour-stamp')
+// const vstamp = require('vigour-stamp')
 
 test('queue', function (t) {
   const server = new Hub({
-    port: 6000,
-    field: { title: 'server' }
+    port: 6000
   })
 
   const client1 = new Hub({
+    url: 'ws://localhost:6000',
     context: 'blurf',
-    id: 1,
-    url: 'ws://localhost:6000'
+    id: 1
   })
 
   const client2 = new Hub({
     url: 'ws://localhost:6000',
     context: 'blurf',
-    id: 2,
-    field: {
-      lulz: true
-    }
+    id: 2
   })
 
-  client1.set({
-    field: {
-      title: 'client-1'
-    }
+  client1.set({ a: 1, b: 1 })
+  client2.set({ a: 2, b: 2 })
+
+  server.get('a', {}).on(function (val, stamp) {
+    console.log('incoming', val, stamp, this.parent.context)
   })
 
-  client1.set({
-    field: {
-      title: 'client-1!'
-    }
+  isConnected(true, () => {
+    const context = server.getContext('blurf')
+    t.equal(context.a.val, 2, '"a" is set by "client2"')
+    t.equal(context.b.val, 2, '"b" is set by "client2"')
+    server.port.set(false)
+    isConnected(false, disconnect)
   })
 
-  client2.set({
-    field: {
-      title: 'client-2!'
-    }
-  })
+  function disconnect () {
+    t.ok(true, 'disconnected clients')
+    server.port.set(6000)
+    isConnected(true, reconnect)
+  }
 
-  console.log(client1.queue)
-  console.log(client2.queue)
+  function reconnect () {
+    t.ok(true, 'reconnected clients')
+    server.remove()
+    client1.remove()
+    client2.remove()
+    t.end()
+  }
 
-  setTimeout(() => {
-    // console.log(server)
-    console.log(server.instances)
-    server.getContext('blurf').clients.on('data', (val, stamp) => {
-      vstamp.done(stamp, () => {
-        console.log('clients--->', server.getContext('blurf').clients)
-      })
-    })
-    client2.url.set('ws://bla.com')
-  }, 500)
-
-  client1.connected.once(() => {
-    console.log('connected! client1')
-  })
-
-  client2.connected.once(() => {
-    console.log('connected! client2')
-  })
+  function isConnected (val, cb) {
+    function error (err) { throw err }
+    Promise.all([
+      client1.connected.is(val),
+      client2.connected.is(val)
+    ]).then(() => {
+      setTimeout(cb, 10)
+    }).catch(error)
+  }
 })
