1a61ccba4d106c47d6dd88d5cb1da6048ac786f6
Jim de Beer <jim@vigour.io>
2016-07-27 09:35:39 +0300

    added clients test

--- test/queue.js
+++ test/queue.js
@@ -4,25 +4,28 @@ const Hub = require('../')
 const vstamp = require('vigour-stamp')
 
 test('queue', function (t) {
-  const server = new Hub({
-    port: 6000
-  })
+  const subs = {
+    $any: { val: true },
+    clients: { $any: { val: true } }
+  }
+
+  const server = new Hub({ port: 6000 })
 
   const client1 = new Hub({
+    id: 1,
     url: 'ws://localhost:6000',
-    context: 'blurf',
-    id: 1
+    context: 'blurf'
   })
 
-  client1.subscribe({ $any: { val: true } })
+  client1.subscribe(subs)
 
   const client2 = new Hub({
+    id: 2,
     url: 'ws://localhost:6000',
-    context: 'blurf',
-    id: 2
+    context: 'blurf'
   })
 
-  client2.subscribe({ $any: { val: true } })
+  client2.subscribe(subs)
 
   client1.set({ a: 1, b: 1, c: 1 })
   client2.set({ a: 2, b: 2, c: 2 })
@@ -32,6 +35,9 @@ test('queue', function (t) {
     t.equal(context.a.val, 2, '"a" is set by "client2"')
     t.equal(context.b.val, 2, '"b" is set by "client2"')
     t.equal(context.c.val, 2, '"c" is set by "client2"')
+    t.same(context.clients.keys(), [ '1', '2' ], 'server has clients')
+    t.same(client1.clients.keys(), [ '1', '2' ], 'client1 has clients')
+    t.same(client2.clients.keys(), [ '2', '1' ], 'client2 has clients')
     server.port.set(false)
     isConnected(false, disconnect)
   })
@@ -40,16 +46,16 @@ test('queue', function (t) {
     t.ok(true, 'disconnected clients')
     client1.a.set(-1)
     client1.b.set(-1)
+    t.same(client1.clients.keys(), [ '1' ], 'client1 does not have client2')
+    t.same(client2.clients.keys(), [ '2' ], 'client2 does not have client1')
     setTimeout(() => {
       client2.a.set(-2)
       client2.b.set(-2)
       setTimeout(() => {
         client1.a.set('a')
         let s = vstamp.create('SPESH')
-        client1.c.remove()
-        // console.log(s)
+        // client1.c.remove()
         vstamp.close(s)
-        console.log(' \n go reconnect!')
         server.port.set(6000)
         isConnected(true, reconnect)
       }, 50)
@@ -57,24 +63,27 @@ test('queue', function (t) {
   }
 
   function reconnect () {
-    t.ok(true, 'reconnected clients')
     const context = server.getContext('blurf')
-    t.equal(context.a.val, 'a', 'server - "a" is set to client1')
-    t.equal(context.b.val, -2, 'server - "b" is set to client2')
-    t.equal(context.c, null, 'server - "c" is removed')
-
-    t.equal(client1.a.val, 'a', 'client1 - got "a" from client1')
-    t.equal(client1.b.val, -2, 'client1 - got "b" from client2')
-    t.equal(client1.c, null, 'client1 - "c" is removed')
-
-    t.equal(client2.a.val, 'a', 'client2 - got "a" from client1')
-    t.equal(client2.b.val, -2, 'client2 - got "b" from client2')
-    t.equal(client2.c, null, 'client2 - "c" is removed')
-
-    server.remove()
-    client1.remove()
-    client2.remove()
-    t.end()
+    t.ok(true, 'reconnected clients')
+    client1.c.remove()
+    Promise.all([
+      context.c.is(null),
+      client2.c.is(null)
+    ]).then(() => {
+      t.equal(context.a.val, 'a', 'server - "a" is set to client1')
+      t.equal(context.b.val, -2, 'server - "b" is set to client2')
+      t.equal(context.c, null, 'server - "c" is removed')
+      t.equal(client1.a.val, 'a', 'client1 - got "a" from client1')
+      t.equal(client1.b.val, -2, 'client1 - got "b" from client2')
+      t.equal(client1.c, null, 'client1 - "c" is removed')
+      t.equal(client2.a.val, 'a', 'client2 - got "a" from client1')
+      t.equal(client2.b.val, -2, 'client2 - got "b" from client2')
+      t.equal(client2.c, null, 'client2 - "c" is removed')
+      server.remove()
+      client1.remove()
+      client2.remove()
+      t.end()
+    })
   }
 
   function isConnected (val, cb) {
