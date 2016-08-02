c75e120de75e155e38eb35db51f1ee9f638e2f64
Jim de Beer <jim@vigour.io>
2016-08-02 07:49:34 +0300

    add upstream to client

--- test/clients.js
+++ test/clients.js
@@ -1,15 +1,59 @@
 'use strict'
 const test = require('tape')
 const Hub = require('../')
-const vstamp = require('vigour-stamp')
+// const vstamp = require('vigour-stamp')
 
 test('clients', function (t) {
   const subs = {
-    $any: { val: true },
-    special: {
-      reference: { val: true }
-    },
     clients: { $any: { val: true } }
   }
+
+  const server = new Hub({
+    id: 'server',
+    port: 6000
+  })
+
+  const hybrid = new Hub({
+    id: 'hybrid',
+    context: false,
+    url: 'ws://localhost:6000',
+    port: 6001
+  })
+
+  const client = new Hub({
+    id: 'client',
+    context: false,
+    url: 'ws://localhost:6001',
+    x: true
+  })
+
+  // client.subscribe(subs)
+  // has to be auto soon -- also unsubscribe
+  hybrid.subscribe(subs)
+
+  server.get('x', {}).is(true).then(() => {
+    console.log('server :)')
+  })
+
+  hybrid.get('x', {}).is(true).then(() => {
+    console.log('hybrid :)')
+  })
+
+  Promise.all([
+    client.connected.is(true),
+    hybrid.connected.is(true),
+    server.get('clients', {}).is(
+      (val, data, stamp, clients) => {
+        console.log(stamp)
+        return clients.keys().length > 1
+      }
+    )
+  ]).then(() => {
+    console.log('got all clients! :D')
+    console.log(server.clients.keys())
+    // console.log(server.clients.client)
+    // console.log('x', server.clients)
+  })
+
   t.end()
 })
