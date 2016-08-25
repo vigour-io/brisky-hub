fe2042be84e234f14f5df60d5395ffa1a05fceb8
Jim de Beer <jim@vigour.io>
2016-08-25 11:45:53 +0200

    renamed to brisky-hub

--- test/clients.js
+++ test/clients.js
@@ -2,14 +2,20 @@
 const test = require('tape')
 const Hub = require('../')
 
-test('clients', function (t) {
-  t.plan(7)
+test('clients', { timeout: 2e3 }, (t) => {
+  t.plan(11)
+
+  // make need a way to not sync clients upstream super important for the scraper setup (else it gets ALL clients)
+  // at the other hand it will work with contexts so its pretty ok
+
   const subs = {
     clients: {
       $any: {
         val: true,
         upstream: { val: true },
-        ip: { val: true }
+        ip: { val: true },
+        device: { val: true },
+        platform: { val: true }
       }
     },
     $any: { val: true }
@@ -31,7 +37,7 @@ test('clients', function (t) {
   const client = new Hub({
     id: 'client',
     context: false,
-    url: 'ws://localhost:6001',
+    url: 'ws://localhost:6001', // upstream has to be scoped to one context -- default is false -- we dont need upstreams for context atm
     x: true
   })
 
@@ -56,6 +62,10 @@ test('clients', function (t) {
     client.client.origin().get('ip', {})
   ]).then(() => {
     t.same(server.clients.keys(), [ 'client', 'hybrid' ], 'server got all clients')
+    t.equal(client.client.origin().device.compute(), 'server', 'client receives correct device type')
+    t.equal(client.client.origin().platform.compute(), 'node.js', 'client receives correct platform')
+    t.equal(server.clients.client.device.compute(), 'server', 'server recieves correct client device type')
+    t.equal(server.clients.client.platform.compute(), 'node.js', 'server recieves correct client platform')
     server.get('x', {}).is(true).then(() => {
       t.ok(true, 'server got x from client')
       disconnect()
