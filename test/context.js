7194c71df5e755453b0d02fa7b3ce4f9dde762ba
Jim de Beer <jim@vigour.io>
2016-08-26 11:06:30 +0200

    create clients, remove handling, block context updates

--- test/context.js
+++ test/context.js
@@ -52,34 +52,39 @@ test('context', function (t) {
     client.set({ context: 'someuser', hello: true })
     server.on(function context (val, stamp) {
       if (val.context) {
-        t.equal(this.context.compute(), 'someuser', 'server received context')
-        t.equal(server.instances.length, 1, 'server has an extra instance')
-        const instance = server.instances[0]
-        t.ok(instance.clients !== server.clients, 'created new clients object for instance')
-        t.same(instance.clients.keys(), [ 'client1' ], 'instance has client1')
-        t.same(server.clients.keys(), [ 'client2', 'client3', 'client4' ], 'client1 removed from server clients')
-        vstamp.done(stamp, () => this.off(context))
-        t.ok(!('hello' in client2), 'does not recieve hello in client2')
-        client2.set({ context: 'someuser' })
-        client2.get('hello', false).is(true).then(() => {
-          t.ok('received context on client2 after switching to someuser')
-          t.ok(!('hello' in client3), 'does not recieve hello in client3')
-          client3.set({ originfield: true })
-          Promise.all([
-            client2.get('originfield', false).is(true),
-            client.get('originfield', false).is(true)
-          ]).then(orginUpdate)
-        })
+        setTimeout(() => {
+          t.equal(this.context.compute(), 'someuser', 'server received context')
+          t.equal(server.instances.length, 1, 'server has an extra instance')
+          const instance = server.instances[0]
+          t.ok(instance.clients !== server.clients, 'created new clients object for instance')
+          t.same(instance.clients.keys(), [ 'client1' ], 'instance has client1')
+
+          console.log(server.clients, instance.clients)
+          t.same(server.clients.keys(), [ 'client2', 'client3', 'client4' ], 'client1 removed from server clients')
+          this.off(context)
+          t.ok(!('hello' in client2), 'does not recieve hello in client2')
+          client2.set({ context: 'someuser' })
+          client2.get('hello', false).is(true).then(() => {
+            t.ok('received context on client2 after switching to someuser')
+            t.ok(!('hello' in client3), 'does not recieve hello in client3')
+            client3.set({ originfield: true })
+            Promise.all([
+              client2.get('originfield', false).is(true),
+              client.get('originfield', false).is(true)
+            ]).then(orginUpdate)
+          })
+        }, 500)
       }
     })
 
     function orginUpdate () {
-      t.ok(true, 'got orgiin field on context clients')
+      t.ok(true, 'got origin field on context clients')
+      console.log(' \n go go go')
       client3.set({ hello: 'bye' })
       client4.get('hello', false).is('bye').then(() => {
         t.ok(true, 'client4 recieves update from client3')
         process.nextTick(() => {
-          console.log(client.hello.compute())
+          // console.log(client.hello.compute())
           t.equal(client.hello.compute(), true, 'client does not get update for hello')
           t.equal(client2.hello.compute(), true, 'client2 does not get update for hello')
           client3.hello.remove()
