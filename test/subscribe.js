140cd28713dc473b0dd118a954bacdae54ebb4f2
Jim de Beer <jim@vigour.io>
2016-09-28 11:27:31 +0200

    some comments to later parse functions

--- test/subscribe.js
+++ test/subscribe.js
@@ -4,29 +4,22 @@ const Hub = require('../')
 
 test('subscribe - exec function gaurds', function (t) {
   const server = new Hub({ port: 6000 })
-
   const client = new Hub({
     id: 1,
     url: 'ws://localhost:6000',
     context: false
   })
-
   client.subscribe({
     bla: {
       $test: {
-        $: {
-          title: { val: true }
-        },
-        $pass: {
-          title: { val: true }
-        },
+        $: { title: { val: true } },
+        $pass: { title: { val: true } },
         exec (state, subs, tree, key) {
           return state.blabla.gurken === true
         }
       }
     }
   })
-
   server.once('error', (err) => {
     t.equal(err.message.indexOf('cannot run function $test.exec'), 0, 'throws run error for false exec')
     const subsHash = Object.keys(client.subscriptions)[0]
