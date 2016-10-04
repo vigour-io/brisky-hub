1551ae0612a9ee31cedddc80e0915874299016a3
Jim de Beer <jim@vigour.io>
2016-10-04 16:12:53 +0200

    added poc for tree reuse

--- test/tree.js
+++ test/tree.js
@@ -4,17 +4,36 @@ const Hub = require('../')
 // const vstamp = require('vigour-stamp')
 
 test('tree - exec function gaurds', (t) => {
-  const server = new Hub({ port: 6000 })
+  const server = new Hub({
+    id: 'server',
+    port: 6000,
+    a: 'hello a'
+  })
 
-  const subs = {}
+  const subs = {
+    a: { val: true }
+  }
 
   const client = new Hub({
     id: 1,
-    context: false
-  })
-  const client1 = new Hub({
-    id: 2,
-    context: false
+    context: false,
+    url: 'ws://localhost:6000'
   })
 
+  client.subscribe(subs)
+
+  const client1 = new Hub({
+    id: 2,
+    context: false,
+    url: 'ws://localhost:6000'
+  })
+
+  client1.subscribe(subs)
+
+  setTimeout(() => {
+    client1.remove()
+    client.clients.is(() => client.clients.keys().length === 1).then(() => {
+      client.remove()
+    })
+  }, 100)
 })
