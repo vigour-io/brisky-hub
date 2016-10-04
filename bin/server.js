3e5df9f3311784720aefb270439e1e9a47faa107
Mustafa Dokumaci <mustafa@foraa.com>
2016-10-04 17:10:30 +0200

    standard fix

--- bin/server.js
+++ bin/server.js
@@ -4,7 +4,4 @@
 
 const Hub = require('../lib')
 
-const hub = new Hub({
-  id: process.argv[2] || 'hub',
-  port: process.argv[3] || 3031
-})
+new Hub({ id: process.argv[2] || 'hub', port: process.argv[3] || 3031 }) // eslint-disable-line
