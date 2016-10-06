f40340d0fb8ed2bb9c6e6594d40f73c2d61daa80
Jim de Beer <jim@vigour.io>
2016-10-06 12:03:34 +0200

    remove reuse

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
 'use strict'
 require('./queue')
-// require('./clients')
-// require('./context') // not finished!
-// require('./sync')
-// require('./references')
-// require('./subscribe')
-// require('./reconnect')
+require('./clients')
+require('./context') // not finished!
+require('./sync')
+require('./references')
+require('./subscribe')
+require('./reconnect')
